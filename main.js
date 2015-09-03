c24baa6f6d4be372ac17a3b6324d51873b6d99ba
Slava Kim <slava@meteor.com>
2015-09-03 13:24:18 -0400

    some basic parsing

--- main.js
+++ main.js
@@ -1,6 +1,79 @@
 import fs from 'fs-promise';
 import minimist from 'minimist';
 import org from 'org';
+import assign from 'object-assign';
+
+class Node {
+  constructor({type, parent, text, attrs}) {
+    this.type = type;
+    this.parent = parent || null;
+    this.text = text || null;
+    this.attrs = attrs || {};
+    this.children = [];
+  }
+
+  addHeadingNode(text) {
+    this.children.push(
+      new HeadingNode({text, parent: this}));
+  }
+
+  lastChild() {
+    return this.children[this.children.length - 1];
+  }
+
+  prettyPrint(level = 0) {
+    console.log(`${pad(level)} #${this.type} ${this.text} ${JSON.stringify(this.attrs)}`);
+    this.children.forEach(child => child.prettyPrint(level + 1));
+  }
+}
+
+class HeadingNode extends Node {
+  constructor({text, parent}) {
+    const type = 'heading';
+    const {attrs, parsedText} = parseHeadingText(text);
+    super({text: parsedText, type, parent, attrs});
+  }
+
+  addAttrs(attrs) {
+    this.attrs = assign(this.attrs, attrs);
+  }
+
+  addTextNode(text) {
+    this.children.push(
+      new TextNode({text, parent: this}));
+  }
+}
+
+class TextNode extends Node {
+  constructor({text, parent}) {
+    const type = 'text';
+    super({type, parent, text});
+  }
+
+  addHeadingNode() {
+    throw new Error('bad idea?');
+  }
+  addTextNode() {
+    throw new Error('bad idea?');
+  }
+}
+
+function parseHeadingText(text) {
+  const attrs = {};
+  let parsedText = text;
+
+  if (text.match(/^TODO /)) {
+    attrs.todo = 'todo';
+    parsedText = text.slice('TODO '.length);
+  } else if (text.match(/^DONE/)) {
+    attrs.todo = 'done';
+    parsedText = text.slice('DONE '.length);
+  } else {
+    attrs.todo = null;
+  }
+
+  return {attrs, parsedText};
+}
 
 function pad(num) {
   return new Array(num + 1).join(' ');
@@ -8,15 +81,33 @@ function pad(num) {
 
 // expects `this` to be the current mutable tree
 function walk(node, level = 1) {
-  console.log(`${pad(level * 2)}${node.type}(${node.level || '-'}) - ${node.value || 'N/A'}`);
-  node.children.forEach(n => walk(n, level + 1));
+  //console.log(`${pad(level * 2)}${node.type}(${node.level || '-'}) - ${node.value || 'N/A'}`);
+  const heading = this.lastChild();
+  if (node.type === 'header') {
+    this.addHeadingNode(node.children[0].children[0].value);
+  } else if (node.type === 'inlineContainer') {
+    heading.addTextNode(node.children[0].value);
+  } else if (node.type === 'paragraph') {
+    heading.addTextNode(node.children[0].children[0].value);
+  }
 }
 
 async function main (filepath) {
-  const parser = new org.Parser();
-  const orgCode = await fs.readFile(filepath, 'utf8');
-  const orgDocument = parser.parse(orgCode);
-  orgDocument.nodes.forEach(n => walk(n));
+  try {
+    const parser = new org.Parser();
+    const orgCode = await fs.readFile(filepath, 'utf8');
+    const orgDocument = parser.parse(orgCode);
+
+    const ast = new Node({
+      type: 'root'
+    });
+
+    orgDocument.nodes.forEach(n => walk.call(ast, n));
+    ast.prettyPrint();
+  } catch (err) {
+    console.log('main async code threw an error:');
+    console.log(err.stack);
+  }
 }
 
 const argv = minimist(process.argv.slice(2));
